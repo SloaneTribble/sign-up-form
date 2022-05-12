cf1f96470fc710e79e5cba81022422eba7237ced
Sloane Tribble <SloaneTribble@gmail.com>
2022-05-12 15:34:04 -0700

    Add responsivity to inputs

--- javascript.js
+++ javascript.js
@@ -2,13 +2,18 @@ let password = document.getElementById("password");
 
 let confirmPassword = document.getElementById("confirm-password");
 
-function validate(){
+function validatePassword(){
     if(password.value != confirmPassword.value){
-        confirmPassword.setCustomValidity("Passwords Don't Match")
+        confirmPassword.setCustomValidity("Passwords Don't Match");
+        password.style.borderColor = "red";
+        confirmPassword.style.borderColor = "red";
     } else {
         confirmPassword.setCustomValidity('');
+        password.style.borderColor = "white";
+        confirmPassword.style.borderColor = "white";
     }
 }
 
-password.onchange = validate;
-confirmPassword.onkeyup = validate;
+password.onchange = validatePassword;
+confirmPassword.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
